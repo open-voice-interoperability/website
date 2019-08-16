dad411d8b2363a64ef65051b585057699e5b7d2d
Dan Cundiff <dan.cundiff@target.com>
2019-08-16 10:34:20 -0500

    Replace farmer photo on home - closes #11

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -52,7 +52,7 @@ export default class PostPreview extends React.Component {
               </div>
 
               <div className="w-60-l ph2-l">
-                <img src="/img/home-about-section.jpg" alt="" className="mb3"/>
+                <img src="/img/josh-hild-mo1lxrlelfs-unsplash-.jpg" alt="" className="mb3"/>
               </div>
             </div>
 
