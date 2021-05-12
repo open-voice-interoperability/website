86d3288cb00bae4d345477c5d7399ff8c3dfc341
Sam Stevens <sam@ctrl-alt-sam.com>
2021-05-12 15:35:54 -0500

    add document content type

--- src/js/cms.js
+++ src/js/cms.js
@@ -6,6 +6,7 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
+import DocumentPreview from "./cms-preview-templates/document";
 import SponsoringMemberPreview from "./cms-preview-templates/sponsoring_member";
 import SupportersPreview from "./cms-preview-templates/supporters";
 import JoinPreview from "./cms-preview-templates/join";
@@ -20,6 +21,7 @@ import VoiceInteroperabilityPreview from "./cms-preview-templates/voice_interope
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
+CMS.registerPreviewTemplate("docs", DocumentPreview);
 CMS.registerPreviewTemplate("sponsoring_member", SponsoringMemberPreview);
 CMS.registerPreviewTemplate("supporters", SupportersPreview);
 CMS.registerPreviewTemplate("join", JoinPreview);
