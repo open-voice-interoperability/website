7eb8f86d17174ba4fb964370e271b655459b9b5a
donald.osgood@phrehab.com <donald.osgood@phrehab.com>
2022-11-14 15:22:48 -0600

    add new menu items

--- newpage.sh
+++ newpage.sh
@@ -3,11 +3,12 @@ if [[ $# -eq 0 ]] ; then
     echo 'Please enter a page name'
     exit 1
 fi
-pagename=$1
-echo "Copy About to $pagename"
-cp -r site/content/about/ site/content/$pagename/
-cp site/layouts/section/about.html site/layouts/section/$pagename.html
-cp src/js/cms-preview-templates/about.js src/js/cms-preview-templates/$pagename.js
+copyfrom=$1
+pagename=$2
+echo "Copy $copyfrom to $pagename"
+cp -r site/content/$copyfrom/ site/content/$pagename/
+cp site/layouts/section/$copyfrom.html site/layouts/section/$pagename.html
+cp src/js/cms-preview-templates/$copyfrom.js src/js/cms-preview-templates/$pagename.js
 echo "Open $pagename files for editing"
 code site/static/admin/config.yml
 code src/js/cms.js
